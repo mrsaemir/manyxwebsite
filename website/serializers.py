299e4cb6f2d911be6dc5b5945c9bce060b45513d
Amirhossein Saemi <saemiamirhossein@gmailc.com>
2018-10-25 10:26:56 +0330

    added some features to ManyxProject common api

--- website/serializers.py
+++ website/serializers.py
@@ -5,8 +5,13 @@ from .models import ManyxProject
 # serializer for demonstration on main page
 # this serializer is readonly.
 class ManyxProjectCommonSerializer(serializers.ModelSerializer):
+    under_development = serializers.SerializerMethodField()
 
     class Meta:
         model = ManyxProject
         # only insensitive info is available through the serializer
-        fields = ('title', 'start_date', 'end_date', 'live_url')
+        fields = ('title', 'snapshot', 'start_date', 'end_date', 'live_url', 'under_development')
+
+    @staticmethod
+    def get_under_development(obj):
+        return obj.under_development
