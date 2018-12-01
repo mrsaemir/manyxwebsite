29af375a0dfe07d12f9b05ba0d8ae2f3d72bfa4b
Amirhossein Saemi <saemiamirhossein@gmailc.com>
2018-12-01 20:21:20 +0330

    added Browsable APIRenderer to settings

--- manyx/settings.py
+++ manyx/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
 
