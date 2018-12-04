d93802457e0ee011088073662f878ed479e477e3
Amirhossein Saemi <saemiamirhossein@gmailc.com>
2018-12-04 13:41:38 +0330

    fixed an issue in blog's url module

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,4 @@ from rest_framework.routers import DefaultRouter
 from .views import ManyxBlogCommonViewSet
 
 router = DefaultRouter()
-router.register(r'blog', ManyxBlogCommonViewSet, base_name='blog')
+router.register(r'', ManyxBlogCommonViewSet, base_name='blog')
