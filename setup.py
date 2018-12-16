a686510d3388b57411047de73fb85ce1387328a3
Amirhossein Saemi <saemiamirhossein@gmailc.com>
2018-12-16 15:28:11 +0330

    added set_secrets

--- setup.py
+++ setup.py
@@ -11,12 +11,20 @@ def create_dir_in_path(dir_name, path_):
     return dir_path
 
 
+def set_secrets():
+    with open('/manyx/secret', 'rw') as file:
+        file_data = file.read()
+        file_data = file_data.replace('some_user', raw_input("DataBase Username:"))
+
+
 def deploy():
     # installing required packages
     os.system("apt update ; apt install docker docker.io docker-compose")
     # initializing secret file
     create_dir_in_path("manyx", "/")
     shutil.copy(os.path.join(os.path.abspath(os.path.dirname(__file__)), "secret"), "/manyx/secret")
+    # configuring env var files
+    set_secrets()
     os.system("docker-compose up --build -d")
     os.system("docker-compose exec web python manage.py migrate --noinput")
     os.system("docker-compose exec web python manage.py collectstatic --noinput")
