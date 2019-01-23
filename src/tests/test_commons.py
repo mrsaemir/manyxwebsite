6dc00313535a09b7fae8d9e4e40912a90d0a9bef
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-01-23 14:37:12 -0500

    added auth token generator check to FTs

--- src/tests/test_commons.py
+++ src/tests/test_commons.py
@@ -1,7 +1,7 @@
 import os
-from django.test import LiveServerTestCase, Client
+from django.test import LiveServerTestCase
 from django.conf import settings
-
+from rest_framework.test import APIClient
 
 class TestCommons(LiveServerTestCase):
     # check that browsable api renderer is not activated in REST_FRAMEWORK_SETTINGS.
@@ -16,3 +16,9 @@ class TestCommons(LiveServerTestCase):
     def test_settings(self):
         self.assertTrue(os.path.exists(settings.STATIC_ROOT), "STATIC_ROOT directory is not set.")
         self.assertTrue(os.path.exists(settings.MEDIA_ROOT), "MEDIA_ROOT directory is not set.")
+
+    # check if auth generator is available.
+    def test_auth_token_generator(self):
+        client = APIClient()
+        res = client.get(self.live_server_url + "/api/token/")
+        self.assertEqual(res.status_code, 405, "Auth token generator is not available.")
