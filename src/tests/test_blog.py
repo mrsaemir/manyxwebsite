ad95324e255df6709acb506b4ec12bdae6d30b65
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-01-27 09:24:54 -0500

    redesigned blog FT's setUp function to work with admin/staff usrs

--- src/tests/test_blog.py
+++ src/tests/test_blog.py
@@ -2,22 +2,39 @@ from django.test import LiveServerTestCase
 from rest_framework.test import APIClient
 import requests
 from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 # testing the main page for blog; testing availability of the main page.
 class TestBlogMainPage(LiveServerTestCase):
-    # returns a logged in client.
-    def gen_admin_client(self):
-        User = get_user_model()
-        user = User.objects.create(username="admin", is_superuser=True, is_staff=True)
-        user.set_password("adminadmin")
-        user.save()
+    # creating both staff and admin users.
+    def setUp(self):
+        admin_user = User.objects.create(username="admin", is_superuser=True, is_staff=True)
+        admin_user.set_password("adminadmin")
+        admin_user.save()
+        self.admin_user = admin_user
         res = requests.post(self.live_server_url + '/api/token/',
                             data={"username": "admin", "password": "adminadmin"}).json()
-        self.token = res['token']
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
-        return client
+        self.admin_token = res['token']
+        self.admin_client = APIClient()
+        self.admin_client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_token)
+
+        # creating staff user.
+        staff_user = User.objects.create(username="staff", is_staff=True)
+        staff_user.set_password("staffstaff")
+        staff_user.save()
+        self.staff_user = staff_user
+        res = requests.post(self.live_server_url + '/api/token/',
+                            data={"username": "staff", "password": "staff"}).json()
+        self.staff_token = res['token']
+        self.staff_client = APIClient()
+        self.staff_client.credentials(HTTP_AUTHORIZATION='Token ' + self.staff_token)
+
+    def create_valid_post_as_staff_user(self):
+        self.fail("write me")
+
+    def create_valid_post_as_admin(self):
+        self.fail("write me")
 
     def test_availability_for_main_page(self):
         client = APIClient()
@@ -38,7 +55,7 @@ class TestBlogMainPage(LiveServerTestCase):
         # checking permissions working properly.
         self.assertEqual(res.status_code, 401, "permissions are not working properly.")
         # lets log into admin page for blog
-        client = self.gen_admin_client()
+        client = self.admin_client
         res = client.get(path=self.live_server_url + '/blog/admin/')
         self.assertEqual(res.status_code, 200, "Can't log into blog's admin page.")
         # check headers
