cbd1de8eaa340c28930bf4cf7572798c5419520d
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-02-06 13:26:58 -0500

    renamed entrypoint.py to development_entrypoint.py

---
+++ src/development_entrypoint.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+User = get_user_model()
+# if admin exists:
+user = User.objects.filter(username="admin")
+if user:
+    user = user[0]
+    user.is_superuser = True
+    user.is_staff = True
+    user.set_password("adminadmin")
+    user.save()
+else:
+    user = User(username='admin')
+    user.is_superuser = True
+    user.is_staff = True
+    user.set_password("adminadmin")
+    user.save()
