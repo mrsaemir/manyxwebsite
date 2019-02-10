54a00c1a148635e421106c73ec81a7c37e5f3398
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-02-10 14:25:24 -0500

    temporarily removed maintenance mode from project

--- src/manyx/middleware.py
+++ src/manyx/middleware.py
@@ -5,17 +5,7 @@ from django.http import HttpResponse
 
 
 # website will be deactivated while it is in maintenance mode.
-# note : in order for this middleware to work it is needed that
-# a reverse proxy(like nginx) sets the appropriate header.(HTTP_X_REAL_IP)
 class MaintenanceMode(MiddlewareMixin):
     @staticmethod
     def process_request(request):
-        if settings.MAINTENANCE:
-            # ip, is_routable = get_client_ip(request)
-            ip = request.META.get("X-Forwarded-For", None)
-            raise IOError(ip)
-            if ip is not None:
-                if ip not in settings.DEBUG_IPS:
-                    return HttpResponse('Under Maintenance! Try Again Later ...', status=503)
-            else:
-                return HttpResponse('Under Maintenance! Try Again Later ...', status=503)
+        pass
