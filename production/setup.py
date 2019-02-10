9023ecd9a5608fb6d3c8ff442dfb9a4f29fb9343
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-02-10 09:39:33 -0500

    bug fixes on production line

--- production/setup.py
+++ production/setup.py
@@ -8,11 +8,6 @@ def pre_deploy():
     os.system("mkdir -p /var/manyx/media")
     os.system("mkdir -p /var/manyx/static")
     os.system("mkdir -p /var/manyx/database")
-    os.system("""echo NOTICE: you have to manually initial your swarm,
-                 edit your production.yaml file and join all the clusters
-                  to your manager node""")
 
 
-if "__name__" == "__main__":
-    pre_deploy()
-
+pre_deploy()
